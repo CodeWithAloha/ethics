a4977c09833cd8c433204a17ba8d916853f5e91a
Russell Vea <russellbvea@gmail.com>
2017-03-04 15:13:48 -1000

    Outputting candidates.

--- js/aggregate.js
+++ js/aggregate.js
@@ -22,11 +22,25 @@ promises.push(new Promise(resolve => {
       const columns =
         meta.view.columns.map(column => column.fieldName)
 
-      console.log(data)
+      const NAME = columns.indexOf('filer')
+      const TITLE = columns.indexOf('title')
+
+      const is_valid = (title => {
+        return title.includes('Legislator') || title.includes('Governor')
+      })
 
       const legislators
-        = [... new Set(data.map(dat => dat[columns.indexOf('filer')]))]
-      resolve(legislators)
+        = [... new Set(
+            data
+              .filter(dat => is_valid(dat[TITLE]))
+              .map(dat => dat[NAME])
+          )]
+
+      resolve(legislators.reduce((obj, leg) => {
+        return Object.assign(obj, {
+          [leg]: data.filter(dat => dat[NAME] == leg)
+        })
+      }, {}))
     })
 }))
 
@@ -55,7 +69,6 @@ promises.push(new Promise(resolve => {
     })
 }))
 
-
 promises.push(new Promise(resolve => {
   fetch('http://www.codeforhawaii.org/ethics/data/campaign-spending.json')
     .then(data => data.json())
@@ -78,18 +91,54 @@ promises.push(new Promise(resolve => {
 
 Promise
   .all(promises)
-  .then(([loans, spending])=> {
+  .then(([gift, loans, spending])=> {
 
     const legislators =
-      [... new Set(Object.keys(loans).concat(Object.keys(spending)))]
+      [... new Set(
+        Object.keys(loans)
+          .concat(Object.keys(spending))
+          .concat(Object.keys(gift))
+        )
+      ]
 
     const data = legislators.reduce((obj, leg) => {
       return Object.assign(obj, {
         [leg]: {
           loans: loans[leg],
-          spending: spending[leg]
+          spending: spending[leg],
+          gift: gift[leg]
         }
       })
     }, {})
+
+    new Vue({
+      el: '.app',
+      data: {
+        data: data
+      },
+      template: `
+      <div>
+        <h1>Contibutors</h1>
+        <article :style="{display: 'flex', flexWrap: 'wrap'}">
+
+          <div v-for="(data, name) in data" :style="{flex: '0 0 25%'}">
+            <h3>{{ name }}</h3>
+            <div>
+              Loans: <span v-if="data.loans">&#10003;</span>
+                     <span v-else>&#10007;</span>
+            </div>
+            <div>
+              Spending: <span v-if="data.spending">&#10003;</span>
+                     <span v-else>&#10007;</span>
+            </div>
+            <div>
+              Gifts: <span v-if="data.gift">&#10003;</span>
+                     <span v-else>&#10007;</span>
+            </div>
+          </div>
+        </article>
+      </div>`
+    })
+
     window.data = data
   })
